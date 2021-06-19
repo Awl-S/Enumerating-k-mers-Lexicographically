7d7f31e659790518557398920278b5af5e46a597
Твоя Булочка <85406297+Awl-S@users.noreply.github.com>
2021-06-20 02:10:28 +0300

    Add files via upload

---
+++ Enumerating k-mers Lexicographically.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+void func(string str, int k) {
+
+    if (k > 0) for (const auto& ch1 : str) {
+        if (k > 1) for (const auto& ch2 : str) {
+            if (k > 2) for (const auto& ch3 : str) {
+                if (k > 3) for (const auto& ch4 : str) {
+                    cout << ch1 << ch2 << ch3 << ch4 << endl;
+                }
+                else cout << ch1 << ch2 << ch3 << endl;
+            }
+            else cout << ch1 << ch2 << endl;
+        }
+        else cout << ch1 << endl;
+    }
+    cout << endl;
+}
+
+int main() {
+    string str; 
+    cin >> str;
+    int k;
+    cin >> k;
+
+    func(str,k);
+
+    return 0;
+}
